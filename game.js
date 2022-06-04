67e85497376f47171e136eb21a7181d94f813d35
saanvi-maheshwari <83226597+saanvi-maheshwari@users.noreply.github.com>
2022-06-04 19:41:08 +0530

    Update game.js

--- game.js
+++ game.js
@@ -33,12 +33,12 @@ get_answer = document.getElementById("input_box").value;
 if(get_answer==actual_answer)
 {
 if(answer_turn=="player1"){
-update_player1_score = player1_score+1;
-document.getElementById("player1_score").innerHTML = update_player1_score;
+update_player_1_score = player1_score+1;
+document.getElementById("p1_score").innerHTML = update_player1_score;
 }
 else{
-    update_player2_score = player2_score+1;
-    document.getElementById("player2_score").innerHTML = update_player2_score;
+ update_player_2_score = player2_score+1;
+    document.getElementById("p2_score").innerHTML = update_player2_score;
 }
 }
 }
